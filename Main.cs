83e11a9467265ffcff21df5ca9dd0d4ca955ef67
Muntajab Aldeen <muntajab_alani@yahoo.com>
2022-01-11 17:15:31 +0300

    change Icon and try..Catch

--- Main.cs
+++ Main.cs
@@ -22,14 +22,6 @@ namespace Sa7kaWin
         private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
         [DllImport("user32.dll")]
         private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
-        enum KeyModifier
-        {
-            None = 0,
-            Alt = 1,
-            Control = 2,
-            Shift = 4,
-            WinKey = 8
-        }
 
         public Main()
         {
@@ -37,12 +29,19 @@ namespace Sa7kaWin
         }
         private void Main_Load(object sender, EventArgs e)
         {
-            this.Text += " " + Application.ProductVersion;
-            _keyString = TxtShortcut.Text = Properties.Settings.Default.KeyString;
-            _selectedKey = (Keys)Enum.Parse(typeof(Keys), _keyString);
-            CbStartApplicationOnStartUp.Checked = System.IO.File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\" +
-                    Application.ProductName + ".lnk");
-            RegisterHotKey(this.Handle, 0, (int)KeyModifier.Shift, _selectedKey.GetHashCode());
+            try
+            {
+                this.Text += " " + Application.ProductVersion;
+                _keyString = TxtShortcut.Text = Properties.Settings.Default.KeyString;
+                _selectedKey = (Keys)Enum.Parse(typeof(Keys), _keyString);
+                CbStartApplicationOnStartUp.Checked = System.IO.File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\" +
+                        Application.ProductName + ".lnk");
+                RegisterHotKey(this.Handle, 0, (int)KeyModifier.Shift, _selectedKey.GetHashCode());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
         private void BtnStart_Click(object sender, EventArgs e)
         {
@@ -93,23 +92,30 @@ namespace Sa7kaWin
         }
         protected override void WndProc(ref Message m)
         {
-            base.WndProc(ref m);
-
-            if (m.Msg == 0x0312)
+            try
             {
-                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
-                if (_start)
-                {
-                    if (key == _selectedKey)
-                    {
-                        SendKeys.SendWait("^A");
-                        SendKeys.SendWait("^X");
-                        Clipboard.SetText(Convert(Clipboard.GetText()));
-                        SendKeys.SendWait("^V");
-                        NotifyIcon.ShowBalloonTip(1000);
+                base.WndProc(ref m);
 
+                if (m.Msg == 0x0312)
+                {
+                    Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+                    if (_start)
+                    {
+                        if (key == _selectedKey)
+                        {
+                            SendKeys.SendWait("^A");
+                            SendKeys.SendWait("^X");
+                            Clipboard.SetText(Convert(Clipboard.GetText()));
+                            SendKeys.SendWait("^V");
+                            NotifyIcon.ShowBalloonTip(1000);
+
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
         private void Main_FormClosing(object sender, FormClosingEventArgs e)
@@ -147,25 +153,32 @@ namespace Sa7kaWin
         }
         private void BtnChangeShortcut_Click(object sender, EventArgs e)
         {
-            if (TxtShortcut.Text == string.Empty)
-                return;
-
-            Properties.Settings.Default.KeyString = _keyString;
-            Properties.Settings.Default.Save();
-
-            _selectedKey = (Keys)Enum.Parse(typeof(Keys), _keyString);
-
-            Task.Run(() =>
+            try
             {
-                LblChanged.Invoke(async () =>
+                if (TxtShortcut.Text == string.Empty)
+                    return;
+
+                Properties.Settings.Default.KeyString = _keyString;
+                Properties.Settings.Default.Save();
+
+                _selectedKey = (Keys)Enum.Parse(typeof(Keys), _keyString);
+
+                Task.Run(() =>
                 {
-                    LblChanged.Visible = true;
-                    await Task.Delay(2500);
-                    LblChanged.Visible = false;
+                    LblChanged.Invoke(async () =>
+                    {
+                        LblChanged.Visible = true;
+                        await Task.Delay(2500);
+                        LblChanged.Visible = false;
+                    });
                 });
-            });
-            UnregisterHotKey(this.Handle, 0);
-            RegisterHotKey(this.Handle, 0, (int)KeyModifier.Shift, _selectedKey.GetHashCode());
+                UnregisterHotKey(this.Handle, 0);
+                RegisterHotKey(this.Handle, 0, (int)KeyModifier.Shift, _selectedKey.GetHashCode());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
         private void QuitToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -183,28 +196,43 @@ namespace Sa7kaWin
 
         private void CbStartApplicationOnStartUp_CheckedChanged(object sender, EventArgs e)
         {
-            string startUpFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
-
-            if (CbStartApplicationOnStartUp.Checked)
+            try
             {
-                WshShell wshShell = new WshShell();
-                IWshShortcut shortcut;
+                string startUpFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
 
-                shortcut =
-                  (IWshShortcut)wshShell.CreateShortcut(
-                    startUpFolderPath + "\\" +
-                    Application.ProductName + ".lnk");
+                if (CbStartApplicationOnStartUp.Checked)
+                {
+                    WshShell wshShell = new WshShell();
+                    IWshShortcut shortcut;
 
-                shortcut.TargetPath = Application.ExecutablePath;
-                shortcut.WorkingDirectory = Application.StartupPath;
-                shortcut.Description = "Launch My Application";
-                shortcut.Save();
+                    shortcut =
+                      (IWshShortcut)wshShell.CreateShortcut(
+                        startUpFolderPath + "\\" +
+                        Application.ProductName + ".lnk");
+
+                    shortcut.TargetPath = Application.ExecutablePath;
+                    shortcut.WorkingDirectory = Application.StartupPath;
+                    shortcut.Description = "Launch My Application";
+                    shortcut.Save();
+                }
+                else
+                {
+                    System.IO.File.Delete(startUpFolderPath + "\\" +
+                        Application.ProductName + ".lnk");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                System.IO.File.Delete(startUpFolderPath + "\\" +
-                    Application.ProductName + ".lnk");
+                MessageBox.Show(ex.Message);
             }
         }
     }
 }
+
+//todo: add translate dll (1)
+//todo: ctrl+ delete to delete word
+//todo: fix Sa7ka only the selected text
+//todo: Salam to سلام can be done by (1)
+//todo: not working on all programs ???
+//todo: put in all characters with and without shift
+//todo: 1-ar to en .. 2-en to ar .. 3-each char from another .. 4-Translate
